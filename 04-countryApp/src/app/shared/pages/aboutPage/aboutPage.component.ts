dc19281651a415200ba63a661e6ee15f85d9c976
ervinsv92 <ervinsv92@gmail.com>
2024-02-22 10:14:43 -0600

    hasta resolucion tarea contryapp

---
+++ 04-countryApp/src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-about-page',
+    templateUrl: './aboutPage.component.html',
+})
+export class AboutPageComponent { }
---
+++ 04-countryApp/src/app/shared/pages/contactPage/contactPage.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-contact-page',
+    templateUrl: './contactPage.component.html',
+    styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class ContactPageComponent { }
