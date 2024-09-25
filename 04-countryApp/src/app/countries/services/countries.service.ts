fb8dafdc55f7e25eff961dc7f3160cf8d1c62511
Ervin <ervinsv92@gmail.com>
2024-09-25 12:04:39 -0600

    hasta mostrar info del pais - 41682361-mostrar-informacion-del-pais

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { catchError, Observable, of } from "rxjs";
+import { catchError, Observable, of, map } from "rxjs";
 import { Country } from "../interfaces/country";
 
 @Injectable({providedIn:'root'})
@@ -10,6 +10,14 @@ export class CountriesService{
 
     constructor(private http:HttpClient){
 
+    }
+
+    searchCountryByAlphaCode(code:string):Observable<Country | null>{
+        return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
+        .pipe(
+            map(countries => countries.length > 0?countries[0]:null),
+            catchError(() => of(null))
+        );
     }
 
     searchCapital(term:string):Observable<Country[]>{
